74406649c4d41f5ca843ad3a98ff52d4486f5db3
odidev <odidev@puresoftware.com>
2020-11-13 10:48:18 +0530

    Update before_install.sh

--- tools/ci/before_install.sh
+++ tools/ci/before_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -v
-
+sudo sed -i 's/127.0.1.1/127.0.0.1/' /etc/hosts
+git config --global user.name "ManageIQ"
+git config --global user.email "contact@manageiq.org"
 source $TRAVIS_BUILD_DIR/tools/ci/setup_vmdb_configs.sh
 source $TRAVIS_BUILD_DIR/tools/ci/setup_ruby_env.sh
 
